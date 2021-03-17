08b574a8a5e6ca559b8610fe4204f14832766f63
phelisa2020 <phelisagqwetha@gmail.com>
2021-03-17 13:43:22 +0200

    first push to netlify

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Global from '.components/Global/Global'
+import './index.css';
+import App from '.componenets/App/App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Global><App /></Global>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
