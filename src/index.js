7b7a150c0e57a1bb15a7fb4d04df21da31f9e2b4
phelisa2020 <phelisagqwetha@gmail.com>
2021-03-18 10:41:16 +0200

    fixing my biulding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Global from '.components/Global/Global'
+import Global from './components/Global/Global'
 import './index.css';
-import App from '.componenets/App/App';
+import App from "./components/App/App";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
